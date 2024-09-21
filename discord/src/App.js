b45be6f1ac4a0445550a92c87630f27a6545c891
rishabh-awa <awasthirishabh52@gmail.com>
2024-09-21 10:46:31 +0530

    correction

--- discord/src/App.js
+++ discord/src/App.js
@@ -2,6 +2,7 @@
 
 import './App.css';
 import {motion} from "framer-motion"
+import Login from './components/loginform';
 
 
 
@@ -10,10 +11,7 @@ import {motion} from "framer-motion"
 function App() {
 
   return (
-  <motion.div  style={{height:"100vh",background:"linear-gradient(90deg,#C31432,#240b3b)"}}
-  >
-
-  </motion.div>
+  <Login />
   );
 }
 
